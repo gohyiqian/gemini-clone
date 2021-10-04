35a8779042252e6b2645a09326ac94029ad9e34f
gyq765 <kenji.gohyiqian@gmail.com>
2021-10-04 16:36:32 +0800

    Basic set up

---
+++ gemini/src/components/Prices/Prices.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Prices = () => {
+  return (
+    <div>
+      <h1>Prices Page</h1>
+      <p>Show all your altcoin prices</p>
+    </div>
+  );
+};
+
+export default Prices;
